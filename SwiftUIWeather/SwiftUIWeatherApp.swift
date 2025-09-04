09df19c5938ced77036cc53cb6b60741e323fde9
Ashish Shah <ashishshah780@gmail.com>
2025-09-04 13:54:41 +0530

    Setup Xcode

---
+++ SwiftUIWeather/SwiftUIWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherApp.swift
+//  SwiftUIWeather
+//
+//  Created by Ashish shah on 04/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
