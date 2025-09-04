a26fcb32dab0a9aaf38c4fd0c5466b213f2e731b
Ashish Shah <ashishshah780@gmail.com>
2025-09-04 20:33:05 +0530

    HomeView

--- SwiftUIWeather/HomeView.swift
+++ SwiftUIWeather/HomeView.swift
@@ -7,18 +7,65 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack(alignment: .center) {
+            bgColor
+            bgView
+            homeImageView
+            weatherInfo
         }
-        .padding()
+    }
+}
+
+extension HomeView {
+    
+    var bgColor: some View {
+        Color.background1
+            .ignoresSafeArea()
+    }
+    
+    var bgView: some View {
+        Image(.background)
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .ignoresSafeArea()
+    }
+    
+    var homeImageView: some View {
+        Image(.house)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(maxHeight: .infinity, alignment: .top)
+            .padding(.top, 300)
+    }
+    
+    var weatherInfo: some View {
+        VStack {
+            Text("Montreal")
+                .font(.largeTitle)
+            VStack {
+                Text("19°")
+                    .font(.system(size: 96, weight: .thin))
+                    .foregroundStyle(.primary)
+                +
+                Text("\n")
+                +
+                Text("Mostly clear")
+                    .font(.title3.weight(.semibold))
+                    .foregroundStyle(.secondary)
+                
+                Text("H:24°   L:18°")
+                    .font(.title3.weight(.semibold))
+            }
+            Spacer()
+        }
+        .foregroundStyle(.white)
+        .padding(.top, 54)
     }
 }
 
 #Preview {
-    ContentView()
+    HomeView()
 }
--- SwiftUIWeather/SwiftUIWeatherApp.swift
+++ SwiftUIWeather/SwiftUIWeatherApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUIWeatherApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
